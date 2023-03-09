95198ba4545eb1e8414c203a31bd96e2ed697819
crl2 <crl2@fresnou.org>
2023-03-09 11:36:14 -0800

    Begin work on user authentication

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const todoController = require('../controllers/todoController');
-
-
-
+const userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/',  todoController.listAll);
-
 
 router.get('/item/add',  todoController.displayAddItem);
 router.post('/item/add',  todoController.addNewItem);
@@ -20,5 +17,7 @@ router.get('/item/delete/:id',  todoController.deleteItem);
 router.get('/item/complete/:id',  todoController.makeItemComplete);
 router.get('/item/incomplete/:id',  todoController.markItemIncomplete);
 
+router.get('/register', userController.renderRegistration);
+router.post('/register', userController.register);
 
 module.exports = router;
