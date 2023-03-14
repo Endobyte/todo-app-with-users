4e19f7bcd7b3d12362378fb1d6f178b052f8f5b8
crl2 <crl2@fresnou.org>
2023-03-14 11:10:42 -0700

    Finish (mostly) authentication

--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,33 @@ function addUserToViews (req, res, next) {
     next();
 }
 
+function redirectGuests (req, res, next) {
+    if (!req.user) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
 /* GET home page. */
-router.get('/',  todoController.listAll);
+router.get('/', addUserToViews, redirectGuests, todoController.listAll);
 
-router.get('/item/add',  todoController.displayAddItem);
-router.post('/item/add',  todoController.addNewItem);
+router.get('/item/add', addUserToViews, redirectGuests, todoController.displayAddItem);
+router.post('/item/add', addUserToViews, redirectGuests, todoController.addNewItem);
 
-router.get('/item/edit/:id',  todoController.viewEditItem);
-router.post('/item/edit/:id',  todoController.saveEditItem);
+router.get('/item/edit/:id', addUserToViews, redirectGuests, todoController.viewEditItem);
+router.post('/item/edit/:id', addUserToViews, redirectGuests, todoController.saveEditItem);
 
-router.get('/item/delete/:id',  todoController.deleteItem);
-router.get('/item/complete/:id',  todoController.makeItemComplete);
-router.get('/item/incomplete/:id',  todoController.markItemIncomplete);
+router.get('/item/delete/:id', addUserToViews, redirectGuests, todoController.deleteItem);
+router.get('/item/complete/:id', addUserToViews, redirectGuests, todoController.makeItemComplete);
+router.get('/item/incomplete/:id', addUserToViews, redirectGuests, todoController.markItemIncomplete);
 
-router.get('/register', userController.renderRegistration);
-router.post('/register', userController.register);
+router.get('/register', addUserToViews, userController.renderRegistration);
+router.post('/register', addUserToViews, userController.register);
 
-router.get('/login', userController.renderLogin);
-router.post('/login', userController.authenticate);
+router.get('/login', addUserToViews, userController.renderLogin);
+router.post('/login', addUserToViews, userController.authenticate);
 
-router.get('/logout', userController.logout);
+router.get('/logout', addUserToViews, userController.logout);
 
 module.exports = router;
